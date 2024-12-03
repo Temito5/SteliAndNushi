a11e0c8e7faa8eb57217acca74dfbf53a9c2875a
1211-13 <1211-13@DESKTOP-PMVUDLD>
2024-12-03 08:38:58 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static async Task<int> CalculateSquareAsync(int number)
+        {
+            Console.WriteLine($"Започвам обработката на числото: {number}");
+
+            await Task.Delay(1000);
+
+            int square = number * number;
+            Console.WriteLine($"Квадратът на {number} е {square}");
+
+            return square;
+        }
+
+        static async Task Main(string[] args)
+        {
+            Console.Write("n= ");
+            int n=int.Parse( Console.ReadLine() );
+
+            List<int> numbers = new List<int>(); 
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                numbers.Add(num);
+            }
+
+            List<Task<int>> tasks = new List<Task<int>>();
+
+            foreach (int number in numbers)
+            {
+                tasks.Add(CalculateSquareAsync(number));
+            }
+
+            int[] results = await Task.WhenAll(tasks);
+
+            Console.WriteLine("Обработката е завършена!");
+        }
+    }
+}
+
+
---
+++ Domashna/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Domashna
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+            string url = "https://jsonplaceholder.typicode.com/todos/1";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string content = await client.GetStringAsync(url); 
+                    Console.WriteLine("Получени данни от уебсайта:");
+                    Console.WriteLine(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Грешка при изтегляне на съдържанието: {ex.Message}");
+            }
+        }
+    }
+}
